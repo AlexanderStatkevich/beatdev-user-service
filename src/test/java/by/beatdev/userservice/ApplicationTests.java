4bdf2b515b4425f808ae004b6f1ec2dbcfcb4e29
AlexanderStatkevich <aastatkevich@gmail.com>
2023-04-10 11:55:16 +0300

    feat tests added

--- src/test/java/by/beatdev/userservice/ApplicationTests.java
+++ src/test/java/by/beatdev/userservice/ApplicationTests.java
@@ -1,13 +1,22 @@
 package by.beatdev.userservice;
 
+import by.beatdev.userservice.controller.UserController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ApplicationTests {
 
+    @Autowired
+    private UserController userController;
+
+
     @Test
     void contextLoads() {
+        assertThat(userController).isNotNull();
     }
 
 }
---
+++ src/test/java/by/beatdev/userservice/integration/IntegrationTest.java
@@ -0,0 +1,63 @@
+package by.beatdev.userservice.integration;
+
+import by.beatdev.userservice.domain.User;
+import by.beatdev.userservice.domain.UserStatus;
+import by.beatdev.userservice.repository.UserRepository;
+import by.beatdev.userservice.service.BlacklistCheckEmailService;
+import by.beatdev.userservice.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@Testcontainers
+public class IntegrationTest {
+    @Container
+    private static final MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.0.32")
+            .withDatabaseName("users")
+            .withUsername("test")
+            .withPassword("test");
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private BlacklistCheckEmailService blacklistCheckEmailService;
+    @Autowired
+    private UserService userService;
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+        registry.add("spring.datasource.password", mySQLContainer::getPassword);
+    }
+
+    @Test
+    void checkGetMethodReturnAppropriateEntity() {
+        User expected = User.builder()
+                .id(1L)
+                .dateTimeCreate(LocalDateTime.now())
+                .dateTimeUpdate(LocalDateTime.now())
+                .email("test@test.test")
+                .fullName("test test")
+                .imageUri("smth")
+                .status(UserStatus.ONLINE)
+                .password("3213")
+                .build();
+        userService.create(expected);
+        User actual = userService.findById(1L);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
